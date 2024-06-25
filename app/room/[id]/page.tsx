32928ed8e1eec60e9e6cc5e13ab299a66b850b63
anurag78787 <tech5@futuristictech.in>
2024-06-25 10:57:48 +0530

    Update page.tsx

--- app/room/[id]/page.tsx
+++ app/room/[id]/page.tsx
@@ -1,8 +1,7 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 import { randomID } from "@/utils/helpers";
-import { useRouter } from 'next/router';
 
 interface Props {
   params: {
@@ -10,60 +9,50 @@ interface Props {
   };
 }
 
-const Page = ({ params }: Props) => {
-  const router = useRouter();
+const page = ({ params }: Props) => {
   const roomID = params.id;
-  const meetingContainerRef = useRef<HTMLDivElement | null>(null);
-  const [username, setUsername] = useState<string | null>(null);
 
-  useEffect(() => {
-    if (router.isReady) {
-      // Get username from query parameters
-      const query = router.query;
-      const usernameFromQuery = Array.isArray(query.username) ? query.username[0] : query.username || "DefaultUsername";
-      setUsername(usernameFromQuery);
-    }
-  }, [router.isReady, router.query]);
+  const myMeeting = (element: HTMLDivElement) => {
+    const appID = process.env.NEXT_PUBLIC_ZEGO_APP_ID;
+    const serverSecret = process.env.NEXT_PUBLIC_ZEGO_SERVER_SECRET || "";
 
-  useEffect(() => {
-    if (username && meetingContainerRef.current) {
-      const myMeeting = (element: HTMLDivElement) => {
-        const appID = process.env.NEXT_PUBLIC_ZEGO_APP_ID;
-        const serverSecret = process.env.NEXT_PUBLIC_ZEGO_SERVER_SECRET || "";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      Number(appID),
+      serverSecret,
+      roomID,
+      randomID(5), // user ID
+      "Anurag" // username
+    );
 
-        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
-          Number(appID),
-          serverSecret,
-          roomID,
-          randomID(5), // user ID
-          username // username from URL
-        );
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+    // start the call
+    zp.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: "Personal link",
+          url: window.location.href,
+          // url:
+          //   window.location.protocol +
+          //   "//" +
+          //   window.location.host +
+          //   window.location.pathname +
+          //   "?roomID=" +
+          //   roomID,
+        },
+        {
+          name: "Meeting ID",
+          url: roomID,
+        },
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+      },
+    });
+  };
 
-        const zp = ZegoUIKitPrebuilt.create(kitToken);
-        // start the call
-        zp.joinRoom({
-          container: element,
-          sharedLinks: [
-            {
-              name: "Personal link",
-              url: window.location.href,
-            },
-            {
-              name: "Meeting ID",
-              url: roomID,
-            },
-          ],
-          scenario: {
-            mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
-          },
-        });
-      };
-
-      myMeeting(meetingContainerRef.current);
-    }
-  }, [username, roomID]);
-
-  return <div className="w-full h-[100vh]" ref={meetingContainerRef}></div>;
+  return <div className="w-full h-[100vh]" ref={myMeeting}></div>;
 };
 
-export default Page;
+export default page;
+
